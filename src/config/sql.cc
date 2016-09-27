12466376e3501424812140611d30e4187420de78
TitanSnow <sweeto@live.cn>
2016-09-27 21:23:22 +0800

    add debug info

--- src/config/sql.cc
+++ src/config/sql.cc
@@ -19,6 +19,9 @@
 
 #include"../../lib/sqlite/sqlite3.h"
 #include<stdlib.h>
+#ifndef NO_KEEPER_LOG
+#include<stdio.h>
+#endif
 #define TDJ_DB_NAME ".tdjconfig.db"
 sqlite3* _inizsql(){
     static struct db_keeper{
@@ -27,9 +30,15 @@ sqlite3* _inizsql(){
             char* dbp=sqlite3_mprintf("%s/" TDJ_DB_NAME,getenv("HOME"));
             if(sqlite3_open(dbp,&db)!=SQLITE_OK) db=0;
             sqlite3_free(dbp);
+#ifndef NO_KEEPER_LOG
+            fputs("db_keeper: construct\n",stderr);
+#endif
         }
         ~db_keeper(){
             sqlite3_close(db);
+#ifndef NO_KEEPER_LOG
+            fputs("db_keeper: destruct\n",stderr);
+#endif
         }
     }dbk;
     return dbk.db;
